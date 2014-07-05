fc0cd0e3dcfa377ad624912f8134f22bd02347e3
Dveim <alexandrolgash@gmail.com>
2014-07-05 22:11:08 +0300

    More elegant version of fsm

--- src/main/scala/com/dveim/states/State.scala
+++ src/main/scala/com/dveim/states/State.scala
@@ -2,26 +2,21 @@ package com.dveim.states
 
 import com.dveim.cards.{Card, Shoe}
 
-sealed trait State {
-  val cash: Int
-  val bet: Int
-  val shoe: Shoe
-  val playerCards: List[Card]
-  val dealerCards: List[Card]
+case class State(cash: Int,
+                 bet: Int,
+                 shoe: Shoe,
+                 playerCards: List[Card],
+                 dealerCards: List[Card]) {
 
-  def transit(s: State): State
+  def this(s: State) = this(s.cash, s.bet, s.shoe, s.playerCards, s.dealerCards)
 
   def getInput: String = io.StdIn.readLine("Your move: ")
 }
 
-case class StartS(numberOfDecks: Int = 4, cash: Int = 100) extends State {
-  override val bet: Int = 0
-  override val playerCards: List[Card] = Nil
-  override val dealerCards: List[Card] = Nil
-  override val shoe: Shoe = new Shoe(numberOfDecks)
-
-  override def transit(s: State): State = {
-    ???
-  }
+trait StartS extends State {
+  def transit = new State(copy(bet = this.bet + 50)) with DealS
 }
 
+trait DealS extends State {
+  def transit = new State(copy(cash = this.cash + 50)) with StartS
+}
