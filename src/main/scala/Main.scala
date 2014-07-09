ea7b2e62e01cdc3544cf6d9775d5719d3e4a2f9f
Dveim <alexandrolgash@gmail.com>
2014-07-10 02:44:20 +0300

    Added information about rules

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,9 +3,17 @@ import states._
 
 object Main {
     def main(args: Array[String]) = {
-        var game: Transition = new State(100, 0, new Shoe(4), Nil, Nil) with StartS
-        //todo write rules (dealer bust = you get 0.5 bet, win = 1 bet, split only if same ranks, ace+ace+ten=22)
+        println("Some rules:")
+        println(" - shoe has 4 decks")
+        println(" - blackjack gives 1.5 bet")
+        println(" - you get 0.5 bet if dealer busts")
+        println(" - you can split only if both cards have same ranks (no Ace + Ten, only Ace + Ace)")
+        println(" - Ace values 11 if score < 11, else 1 (Ace + Ace + Ten == 22)")
+        println(" - dealer can't blackjack")
+        println(" - you can double if score < 11")
+        println("Good luck!\n")
 
+        var game: Transition = new State(100, 0, new Shoe(4), Nil, Nil) with StartS
         while (!(game.cash == 0 && game.bet == 0))
             game = game.transit
 
